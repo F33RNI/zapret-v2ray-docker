8134bd48244143d283128f5949de7d4f4f038ed3
F33RNI <fernlane@proton.me>
2025-02-22 16:50:09 -0500

    feat: blockcheck wrapper script

---
+++ blockcheck.sh
@@ -0,0 +1,43 @@
+#!/usr/bin/env bash
+
+# This file is part of the zapret-v2ray-docker distribution.
+# See <https://github.com/F33RNI/zapret-v2ray-docker> for more info.
+#
+# Copyright (c) 2025 Fern Lane.
+#
+# Permission is hereby granted, free of charge, to any person obtaining a copy
+# of this software and associated documentation files (the "Software"), to deal
+# in the Software without restriction, including without limitation the rights
+# to use, copy, modify, merge, publish, distribute, sublicense, and/or sell
+# copies of the Software, and to permit persons to whom the Software is
+# furnished to do so, subject to the following conditions:
+#
+# The above copyright notice and this permission notice shall be included in
+# all copies or substantial portions of the Software.
+#
+# THE SOFTWARE IS PROVIDED "AS IS", WITHOUT WARRANTY OF ANY KIND, EXPRESS OR
+# IMPLIED, INCLUDING BUT NOT LIMITED TO THE WARRANTIES OF MERCHANTABILITY,
+# FITNESS FOR A PARTICULAR PURPOSE AND NONINFRINGEMENT. IN NO EVENT SHALL THE
+# AUTHORS OR COPYRIGHT HOLDERS BE LIABLE FOR ANY CLAIM, DAMAGES OR OTHER
+# LIABILITY, WHETHER IN AN ACTION OF CONTRACT, TORT OR OTHERWISE, ARISING FROM,
+# OUT OF OR IN CONNECTION WITH THE SOFTWARE OR THE USE OR OTHER DEALINGS IN
+# THE SOFTWARE.
+
+# This script calls internal ./blockcheck.sh script if container is running
+# NOTE: This script must ONLY be executed OUTSIDE the container
+
+# Get container ID
+container_id=$(docker ps | grep zapret-v2ray-docker | tail -n1 | awk '{print $1}')
+if [ -z "$container_id" ]; then
+    echo "ERROR: Container not found"
+    exit 1
+fi
+
+# Check if it's running
+if [ "$(docker container inspect -f '{{.State.Status}}' $container_id)" != "running" ]; then
+    echo "ERROR: Container is not running!"
+    exit 1
+fi
+
+# Call internal script
+docker exec "$container_id" ./blockcheck.sh
---
+++ container_scripts/blockcheck.sh
@@ -0,0 +1,68 @@
+#!/usr/bin/env bash
+
+# This file is part of the zapret-v2ray-docker distribution.
+# See <https://github.com/F33RNI/zapret-v2ray-docker> for more info.
+#
+# Copyright (c) 2025 Fern Lane.
+#
+# Permission is hereby granted, free of charge, to any person obtaining a copy
+# of this software and associated documentation files (the "Software"), to deal
+# in the Software without restriction, including without limitation the rights
+# to use, copy, modify, merge, publish, distribute, sublicense, and/or sell
+# copies of the Software, and to permit persons to whom the Software is
+# furnished to do so, subject to the following conditions:
+#
+# The above copyright notice and this permission notice shall be included in
+# all copies or substantial portions of the Software.
+#
+# THE SOFTWARE IS PROVIDED "AS IS", WITHOUT WARRANTY OF ANY KIND, EXPRESS OR
+# IMPLIED, INCLUDING BUT NOT LIMITED TO THE WARRANTIES OF MERCHANTABILITY,
+# FITNESS FOR A PARTICULAR PURPOSE AND NONINFRINGEMENT. IN NO EVENT SHALL THE
+# AUTHORS OR COPYRIGHT HOLDERS BE LIABLE FOR ANY CLAIM, DAMAGES OR OTHER
+# LIABILITY, WHETHER IN AN ACTION OF CONTRACT, TORT OR OTHERWISE, ARISING FROM,
+# OUT OF OR IN CONNECTION WITH THE SOFTWARE OR THE USE OR OTHER DEALINGS IN
+# THE SOFTWARE.
+
+# This script executes blockcheck.sh from _ZAPRET_DIR_INT directory
+# NOTE: This script must ONLY be executed inside the container
+
+# Check if we're inside the container
+if [[ "$container" != "docker" ]]; then
+    echo "ERROR: This script can ONLY be executed INSIDE the container"
+    exit 126
+fi
+
+# Domains to check (space separated)
+DOMAINS=${DOMAINS:-${DOMAINS_DEFAULT:-x.com}}
+export DOMAINS
+
+# Output file
+log_file="${_LOGS_DIR_INT}/blockcheck.log"
+
+cd $_ZAPRET_DIR_INT
+
+# Stop zapret first
+echo "Stopping zapret"
+"$_ZAPRET_DIR_INT/init.d/sysv/zapret" stop | tee -a "$_ZAPRET_LOG_FILE"
+
+# Run blockcheck.sh in BATCH mode
+echo -e "\nStarting blockcheck in batch mode on domains: $DOMAINS_DEFAULT"
+BATCH=1 \
+    DOMAINS="$DOMAINS" \
+    IPVS=4 \
+    ENABLE_HTTP=0 \
+    ENABLE_HTTPS_TLS12=1 \
+    ENABLE_HTTPS_TLS13=0 \
+    REPEATS=3 \
+    PARALLEL=1 \
+    SCANLEVEL=standard \
+    SKIP_IPBLOCK=1 \
+    SKIP_TPWS=1 \
+    SECURE_DNS=0 \
+    ./blockcheck.sh | tee "$log_file"
+
+echo -e "\nBlockcheck done! Saved into $log_file"
+
+# Start zapret back
+echo -e "\nStarting zapret back"
+"$_ZAPRET_DIR_INT/init.d/sysv/zapret" start | tee -a "$_ZAPRET_LOG_FILE"
